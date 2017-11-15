cdbaa1d360a10140d4e8eb8ebd20c547d57c4354
John Bellizzi <jabellizzi406@gmail.com>
2017-11-15 14:54:29 -0500

    adding in webpack tools

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/js/index.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/html/index.html",
+      filename: "index.html"
+    })
+  ],
+  devtool: 'cheap-module-eval-source-map'
+}
