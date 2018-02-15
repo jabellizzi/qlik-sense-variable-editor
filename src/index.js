afbe384dcfe5c1c6a7ddb0c96c6b2dfd8533faba
John Bellizzi <jabellizzi406@gmail.com>
2018-02-15 09:48:34 -0500

    initial redux-observable commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
+// ========= React/Redux =========
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { createEpicMiddleware } from 'redux-observable';
+
+// ========= Resources =========
+import reducer from './reducers/index';
+import { rootEpic } from './epics/index';
 import './index.css';
-import App from './App';
+import App from './App.js';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const epicMiddleware = createEpicMiddleware(rootEpic);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancers(
+    applyMiddleware(epicMiddleware)
+  )
+)
+
+ReactDOM.render(
+  <Provider store={ store }>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
+
 registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+  doc: {
+    id: null,
+    name: null
+  }
+};
+
+export function variableReducer(state = initialState, action) {
+  return state;
+}
+
+export default variableReducer;
